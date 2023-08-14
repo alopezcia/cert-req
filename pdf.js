12c5217fcf4dccc166f72eae6754f348b02ff335
Adolfo López <alopezcia@gmail.com>
2023-08-14 18:36:42 +0200

    Retoques en el PDF

--- pdf.js
+++ pdf.js
@@ -6,7 +6,7 @@ const doc = new PDFDocument({
         size: 'A4', 
         margins: {
             top: 20,
-            bottom: 20,
+            bottom: 2,
             left: 20,
             right: 40
             }});
@@ -134,40 +134,48 @@ doc.image('public/hoja.png', 0, 0, {
   // ACEPTO  
   doc.text('X', 44, 600);
 
+  // Firma Padre  
+  doc.text('XXXXXXXX', 44, 750);
   
+  // Firma Madre  
+  doc.text('XXXXXXXX', 358, 750);
   
+  // Firma Padre  
+  doc.text('XXXXXXXXXXXXXXXXXXXXX', 63, 820);
+
+  // Firma Madre  
+  doc.text('XXXXXXXXXXXXXXXXXXXXX', 373, 820);
 
 
+// // Add another page
+// doc
+//   .addPage()
+//   .fontSize(25)
+//   .text('Here is some vector graphics...', 100, 100);
 
-// Add another page
-doc
-  .addPage()
-  .fontSize(25)
-  .text('Here is some vector graphics...', 100, 100);
+// // Draw a triangle
+// doc
+//   .save()
+//   .moveTo(100, 150)
+//   .lineTo(100, 250)
+//   .lineTo(200, 250)
+//   .fill('#FF3300');
 
-// Draw a triangle
-doc
-  .save()
-  .moveTo(100, 150)
-  .lineTo(100, 250)
-  .lineTo(200, 250)
-  .fill('#FF3300');
+// // Apply some transforms and render an SVG path with the 'even-odd' fill rule
+// doc
+//   .scale(0.6)
+//   .translate(470, -380)
+//   .path('M 250,75 L 323,301 131,161 369,161 177,301 z')
+//   .fill('red', 'even-odd')
+//   .restore();
 
-// Apply some transforms and render an SVG path with the 'even-odd' fill rule
-doc
-  .scale(0.6)
-  .translate(470, -380)
-  .path('M 250,75 L 323,301 131,161 369,161 177,301 z')
-  .fill('red', 'even-odd')
-  .restore();
-
-// Add some text with annotations
-doc
-  .addPage()
-  .fillColor('blue')
-  .text('Here is a link!', 100, 100)
-  .underline(100, 100, 160, 27, { color: '#0000FF' })
-  .link(100, 100, 160, 27, 'http://google.com/');
+// // Add some text with annotations
+// doc
+//   .addPage()
+//   .fillColor('blue')
+//   .text('Here is a link!', 100, 100)
+//   .underline(100, 100, 160, 27, { color: '#0000FF' })
+//   .link(100, 100, 160, 27, 'http://google.com/');
 
 // Finalize PDF file
 doc.end();
